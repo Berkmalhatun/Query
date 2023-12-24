97ca3506320cb4d894eb20b60acccd1bc1f5b9c2
Berk <116104909+Berkmalhatun@users.noreply.github.com>
2023-12-25 01:04:26 +0300

    Update SolrRepository.java

--- src/main/java/com/berk/repository/SolrRepository.java
+++ src/main/java/com/berk/repository/SolrRepository.java
@@ -5,23 +5,22 @@ import org.apache.solr.client.solrj.SolrClient;
 import org.apache.solr.client.solrj.SolrQuery;
 import org.apache.solr.client.solrj.response.QueryResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.solr.repository.SolrCrudRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-public class SolrRepository {
+public class SolrRepository{
     @Autowired
     private SolrClient solrClient;
 
     public List<Solr> findDocumentsAfterJanuary2020() throws Exception {
         SolrQuery query = new SolrQuery("*:*");
-        query.set("collection", "enocadb");
+         query.set("collection", "enocadb");
         query.setFilterQueries("updatedAt:[1577836800000 TO *]");
         query.setRows(10);
-
         QueryResponse response = solrClient.query(query);
         return response.getBeans(Solr.class);
     }
 }
-
